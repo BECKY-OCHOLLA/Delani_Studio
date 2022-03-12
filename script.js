d43bf7518ca16f6ac7c32769e82df6ebe3dea59e
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-03-13 02:50:23 +0300

    it will add the form functions

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// toggle
 $(document).ready(function(){
     $(".p1").click(function(){
         $(".design").toggle();
         $("#img1").toggle();
+        
     
 
     });
@@ -55,5 +57,37 @@ $(document).ready(function(){
     });
 
 });
+
+
+// hover
+
+$(document).ready(function(){
+    $("#port").hover(function () {
+        $(this).children(".para").fadeToggle(1000, "linear");
+      });
+      
+
+});
+
+
+function validate(){
+    var name=  document.getElementById("name").value
+    var email= document.getElementById("email").value
+    var message= document.getElementById("message").value
+
+    if(name==""|| email==""||message==""){
+        alert("blank fill the data");
+        return false;
+    }else{
+        alert("Hello" +" " + name + " "+"we have received your message. Thank you for reaching out to us. ")
+    }
+}
+
+        
+
+        
+
+        
+
 
 
