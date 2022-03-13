55bd06811c20a1aae926b0d8e6a96389f135776f
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-03-13 15:05:52 +0300

    it will add the toggles

--- script.js
+++ script.js
@@ -1,74 +1,72 @@
-// toggle
+// toggle on design
 $(document).ready(function(){
-    $(".p1").click(function(){
-        $(".design").hide();
+     $(".p1").click(function(){
+        $(".p1").hide();
         $(".design").toggle();
-        $("#img1").toggle();
-        
-    
-
-    });
-
-});
-
-$(document).ready(function(){
-    $(".p2").click(function(){
-        $(".development").toggle();
-        $("#img2").toggle();
-
-    });
-
-});
-
-$(document).ready(function(){
-    $(".p3").click(function(){
-        $(".product").toggle();
-        $("#img3").toggle();
-
-    });
-
-});
-
- $(document).ready(function(){
-    $("#img1").click(function(){
-         $(".design").toggle();
-         $("#img1").toggle();
-    
-
-    });
-
-});
-
- $(document).ready(function(){
-     $("#img2").click(function(){
-      $(".development").toggle();
-        $("#img2").toggle();
+        $(".design").show();
+        $()
     
 
      });
 
  });
-
- $(document).ready(function(){
-     $("#img3").click(function(){
-        $(".product").toggle();
-         $("#img3").toggle();
-    
-
-  });
+ $(".design").click(function(){
+     $(".design").hide();
+     $(".p1").toggle();
 
  });
 
+//  toggle on development
 
-// hover
+ $(document).ready(function(){
+     $(".p2").click(function(){
+         $(".p2").hide();
+         $(".development").toggle();
+         $(".development").show();
+         $()
 
-$(document).ready(function(){
-    $("#port").hover(function () {
-        $(this).children(".para").fadeToggle(1000, "linear");
-      });
-      
+     });
+
+ });
+
+ $(".development").click(function(){
+    $(".development").hide();
+    $(".p2").toggle();
+
+ });
+
+//  toggle on product
+
+ $(document).ready(function(){
+    $(".p3").click(function(){
+        $(".p3").hide();
+        $(".product").toggle();
+        $(".product").show();
+        $()
+
+    });
 
 });
+
+$(".product").click(function(){
+   $(".product").hide();
+   $(".p3").toggle();
+
+});
+
+
+//  hover effect on portfolio
+
+$(document).ready(function(){
+    $("#project").hover(function(){
+      $(this).children("#graph1").stop().animate({"top":300,"opacity":0.1},700);
+      }, function(){
+      $(this).children("graph1").stop().animate({"top":0,"opacity":1},400);
+    });
+  });
+
+
+// form validation{}
 
 
 function validate(){
@@ -82,6 +80,4 @@ function validate(){
     }else{
         alert("Hello" +" " + name + " "+"we have received your message. Thank you for reaching out to us. ")
     }
-}        
-
-
+}
